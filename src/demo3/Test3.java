185bec6753893ca24a440a9370024c8450a919d0
valen <valen@DESKTOP-057E61O.lan>
2023-04-09 18:16:16 -0400

    new commit

---
+++ src/demo3/Test3.java
@@ -0,0 +1,21 @@
+package demo3;
+
+public class Test3 {
+	
+	public static void main(String arg []) {
+		
+		System.out.println("Test3 project3");
+	}
+
+
+	public void  abc() {
+		
+		System.out.println("Test 3");
+	}
+	
+	public void def() {
+		
+		System.out.println("Test 3");
+	}
+	
+}
